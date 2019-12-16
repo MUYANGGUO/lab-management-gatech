3a39774459aab8f8588e8049cb06fa70b2185700
MUYANG GUO <muyangguo@gmail.com>
2019-12-16 13:07:09 -0500

    working on graphql learning, from an example of event creating, query

--- app.js
+++ app.js
@@ -6,18 +6,39 @@ const { buildSchema } = require('graphql');
 const app = express();
 
 
+//will replace this in production, aka use database
+const events = [];
+
 // app.get('/',(req,res,next)=>{
 //     res.send('Hello Muyang, this is a initial test!');
 // });
 
-app.use('/graphql',graphqlHttp({
+//! mark as non-nullable,
+app.use('/graphql',
+    graphqlHttp({
     schema: buildSchema(`
+        type Event {
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String! 
+        }
+
+        input EventInput {
+            title: String!
+            description: String!
+            price: Float!
+            date: String! 
+        }
+
+
         type RootQuery{
-            events: [String!]!
+            events: [Event!]!
         }
 
         type RootMutation{
-            createEvent(name: String): String
+            createEvent(eventInput: EventInput): Event
         }
 
         schema{
@@ -27,19 +48,29 @@ app.use('/graphql',graphqlHttp({
         }
     `),
     //name must match!
+    //resolver functions:
     rootValue: {
         events: () => {
-            return ['event1','event2','event3'];
+            return events;
         },
 
         createEvent: (args)=>{
-            const eventName = args.name;
-            return eventName;
+            const event = {
+                _id: Math.random().toString(),
+                title: args.eventInput.title,
+                description: args.eventInput.description,
+                price: +args.eventInput.price,
+                date: args.eventInput.date
+                // date: new Date().toISOString()
+            };
+            events.push(event);
+            return event;
         }
     },
-    //
     graphiql: true
-}));
+    
+})
+);
 
 app.use(bodyParser.json());
 
