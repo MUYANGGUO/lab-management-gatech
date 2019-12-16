3d3cbe259c653c98b07d3d689cbce0632fc5fd52
MUYANG GUO <muyangguo@gmail.com>
2019-12-16 00:04:05 -0500

    install graphql, express-graphql middleware to the project, tested with a create Event Schema

--- app.js
+++ app.js
@@ -1,12 +1,45 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 
 
-app.get('/',(req,res,next)=>{
-    res.send('Hello Muyang, this is a initial test!');
-})
+// app.get('/',(req,res,next)=>{
+//     res.send('Hello Muyang, this is a initial test!');
+// });
+
+app.use('/graphql',graphqlHttp({
+    schema: buildSchema(`
+        type RootQuery{
+            events: [String!]!
+        }
+
+        type RootMutation{
+            createEvent(name: String): String
+        }
+
+        schema{
+            query: RootQuery
+            mutation: RootMutation
+
+        }
+    `),
+    //name must match!
+    rootValue: {
+        events: () => {
+            return ['event1','event2','event3'];
+        },
+
+        createEvent: (args)=>{
+            const eventName = args.name;
+            return eventName;
+        }
+    },
+    //
+    graphiql: true
+}));
 
 app.use(bodyParser.json());
 
